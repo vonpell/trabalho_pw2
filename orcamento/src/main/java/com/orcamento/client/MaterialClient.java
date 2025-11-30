d040be4683903d359a8d58eaa52547591acd0526
Afonso Pellenz <afpellenz@gmail.com>
2025-11-30 01:05:00 -0300

    fixed orçamento service, all working

--- orcamento/src/main/java/com/orcamento/client/MaterialClient.java
+++ orcamento/src/main/java/com/orcamento/client/MaterialClient.java
@@ -7,11 +7,11 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import com.orcamento.dto.MaterialDTO;
 
-@Path("/materiais")
+@Path("/api/materiais")
 @RegisterRestClient(configKey = "material-api")
 public interface MaterialClient {
 
     @GET
     @Path("/{id}")
-    MaterialDTO buscarPorId(@PathParam("id") Long id);
+    MaterialDTO buscarPorId(@PathParam("id") String id);
 }
--- orcamento/src/main/java/com/orcamento/model/OrcamentoMaoObra.java
+++ orcamento/src/main/java/com/orcamento/model/OrcamentoMaoObra.java
@@ -4,23 +4,23 @@ import java.math.BigDecimal;
 
 public class OrcamentoMaoObra {
     
-    public Long maoObraId;
+    public String maoObraId;
     public String nomeMaoObra;
     public Integer quantidade;
     public BigDecimal precoUnitario;
     public BigDecimal valorTotal;
-    public String unidade;
+    public String unidadeMedida;
 
     public OrcamentoMaoObra() {
     }
 
-    public OrcamentoMaoObra(Long maoObraId, String nomeMaoObra, Integer quantidade, 
-                           BigDecimal precoUnitario, String unidade) {
+    public OrcamentoMaoObra(String maoObraId, String nomeMaoObra, Integer quantidade, 
+                           BigDecimal precoUnitario, String unidadeMedida) {
         this.maoObraId = maoObraId;
         this.nomeMaoObra = nomeMaoObra;
         this.quantidade = quantidade;
         this.precoUnitario = precoUnitario;
-        this.unidade = unidade;
+        this.unidadeMedida = unidadeMedida;
         calcularValorTotal();
     }
 
